74dffc4301b55d5c18aca02e0e0cc35eba19c583
Nevyn Bengtsson <nevyn.jpg@gmail.com>
2022-04-18 16:45:34 +0200

    initial

---
+++ dearimgraph.cpp
@@ -0,0 +1,12 @@
+﻿// dearimgraph.cpp : Defines the entry point for the application.
+//
+
+#include "dearimgraph.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
